126409fdf5eef6dbfc737ec0f0e43e3dc8f98788
solrac97gr <carlosgarcia_97gr@hotmail.com>
2025-03-06 19:34:04 +0100

    add sum

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 use regex::Regex;
 
 fn main() {
-    let regex_patter: &str = r"(?-u:\w){1000}";
-    assert!(Regex::new(regex_patter).is_ok());
+    // Regex
+    let re_add: Regex = Regex::new(r"(\d+)\s?\+\s?(\d+)").unwrap();
+    // User input
+    let mut user_expression: String = String::new();
+    println!("Insert your math expression:");
+    std::io::stdin().read_line(&mut user_expression).unwrap();
+    // Apply operations
+    let caps = re_add.captures(&mut user_expression).unwrap();
+    let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
+    let right_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
+
+    // Show results
+    println!("Result: {}", left_value + right_value);
 }
