cf62af779c5125095436a1f2b8991c20f887a636
solrac97gr <carlosgarcia_97gr@hotmail.com>
2025-03-06 20:08:22 +0100

    add new operations

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
 use regex::Regex;
 
-fn main() {
-    // Regex
-    let re_add: Regex = Regex::new(r"(\d+)\s?\+\s?(\d+)").unwrap();
-    // User input
-    let mut user_expression: String = String::new();
-    println!("Insert your math expression:");
-    std::io::stdin().read_line(&mut user_expression).unwrap();
-    // Apply operations
+fn apply_operation(pattern: Regex, mut expression: String, operation: &str) -> String {
+    if operation.is_empty() {
+        return "".to_string();
+    }
     loop {
-        let caps = re_add.captures(user_expression.as_str());
+        let caps = pattern.captures(expression.as_str());
         if caps.is_none() {
             break;
         }
@@ -20,9 +16,36 @@ fn main() {
         let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
         let right_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
 
-        let sum = left_value + right_value;
-        user_expression = user_expression.replace(caps_expression, &sum.to_string());
+        let res = match operation {
+            "+" => left_value + right_value,
+            "-" => left_value - right_value,
+            "*" => left_value * right_value,
+            "/" => left_value / right_value,
+            _ => 0,
+        };
+        expression = expression.replace(caps_expression, &res.to_string());
     }
+    return expression;
+}
+
+fn main() {
+    // Regex
+    let re_mult: Regex = Regex::new(r"(\d+)\s?\*\s?(\d+)").unwrap();
+    let re_div: Regex = Regex::new(r"(\d+)\s?\/\s?(\d+)").unwrap();
+    let re_add: Regex = Regex::new(r"(\d+)\s?\+\s?(\d+)").unwrap();
+    let re_less: Regex = Regex::new(r"(\d+)\s?\-\s?(\d+)").unwrap();
+
+    // User input
+    let mut user_expression: String = String::new();
+    println!("Insert your math expression:");
+    std::io::stdin().read_line(&mut user_expression).unwrap();
+
+    // Apply operations
+    user_expression = apply_operation(re_mult, user_expression, "*");
+    user_expression = apply_operation(re_div, user_expression, "/");
+    user_expression = apply_operation(re_add, user_expression, "+");
+    user_expression = apply_operation(re_less, user_expression, "-");
+
     // Show results
     println!("Result: {}", user_expression);
 }
